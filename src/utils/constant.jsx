7765490ad35e05b359268e779ed7ced6a98ad64a
Munigala Deekshitha <deekshithagoud1668@gmail.com>
2024-07-15 22:14:41 +0530

    Updated

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -1,7 +1,7 @@
 export const options = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": "b4e3d77916msh9664b06c2e0e98ep1d83e4jsn03951fbf57ae",
+    "x-rapidapi-key": "13b8556098msh20b7b73ec753bddp1740f4jsn900e271616a8",
     "x-rapidapi-host": "real-time-amazon-data.p.rapidapi.com",
   },
 };
